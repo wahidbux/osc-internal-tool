1d41cab92e30d10c08a298efd3fd79e67e099e2f
Ketan1317 <goyalketan1317@gmail.com>
2025-10-08 17:18:44 +0530

    [Backend] -> Fixed The routers and route files

--- apps/server/src/routes/v1/teamMember.route.ts
+++ apps/server/src/routes/v1/teamMember.route.ts
@@ -1,3 +1,12 @@
+import teamMemberController from "../../controller/teamMember.controller";
 import { Router } from "express";
 
 export const teamMemberRouter = Router();
+
+
+teamMemberRouter.post("/create",teamMemberController.createTeamMember);
+teamMemberRouter.post("/remove/:memberId",teamMemberController.removeTeamMember);
+teamMemberRouter.post("/update/:memberId",teamMemberController.updateTeamMember);
+
+teamMemberRouter.get("/team/:teamId",teamMemberController.getTeamMembersByTeam);
+
